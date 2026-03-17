1ca9865d37d673e1f257b55e514ce28ad49e547f
samdsgn-studio <135076370+samdsgn-studio@users.noreply.github.com>
2026-03-17 01:38:07 +0100

    Initial Raycast extension

---
+++ raycast-env.d.ts
@@ -0,0 +1,28 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `extract-notion-id` command */
+  export type ExtractNotionId = ExtensionPreferences & {}
+  /** Preferences accessible in the `search-notion-ids` command */
+  export type SearchNotionIds = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `extract-notion-id` command */
+  export type ExtractNotionId = {}
+  /** Arguments passed to the `search-notion-ids` command */
+  export type SearchNotionIds = {}
+}
+
